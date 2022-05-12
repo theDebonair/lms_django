ef32832a8ce3ba38cd13af822dfb4a666b31b86a
theDebonair <harshit4277448@live.com>
2022-05-12 14:09:09 +0530

    Bug fix.

--- app/views.py
+++ app/views.py
@@ -12,6 +12,7 @@ from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.encoding import force_bytes, force_str
 from django.contrib.auth.decorators import login_required
 from . tokens import generate_token
+from django.db.models import Q
 from . models import *
 from . forms import *
 
