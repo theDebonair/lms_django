05ee63a74114191ebce52f64601807460c25bfec
theDebonair <harshit4277448@live.com>
2022-05-13 16:33:50 +0530

    Update.

--- lms/settings.py
+++ lms/settings.py
@@ -27,7 +27,7 @@ EMAIL_PORT = 587
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'Secret Key Here'
+SECRET_KEY = 'django-insecure-6f%84p1^+bvzcos5=*su44t*+fir7ouy7fm&87_#g3yxuyhrnx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
