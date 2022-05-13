ce0d85d7b8e1faf32c10882632f6ce9e13eac4c7
theDebonair <harshit4277448@live.com>
2022-05-13 16:17:31 +0530

    Update.

--- lms/settings.py
+++ lms/settings.py
@@ -20,7 +20,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'lmsproject016@gmail.com'
-EMAIL_HOST_PASSWORD = 'teamlms@123'
+EMAIL_HOST_PASSWORD = 'teamlms@321'
 EMAIL_PORT = 587
 
 # Quick-start development settings - unsuitable for production
