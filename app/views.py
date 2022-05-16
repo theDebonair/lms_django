031116425a9f0bc7c786a03c7f14053c805e9b86
theDebonair <harshit4277448@live.com>
2022-05-16 19:25:36 +0530

    Various fixes, improved design.

--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
-from base64 import urlsafe_b64decode
 from pickle import FALSE
-from django.core.mail import EmailMessage, send_mail
+from django.core.mail import send_mail
 from lms import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.sites.shortcuts import get_current_site
 from django.template.loader import render_to_string
@@ -16,12 +15,15 @@ from django.db.models import Q
 from . models import *
 from . forms import *
 
+
 def index(request):
     return render(request, 'app/index.html')
+
 
 @login_required(login_url='/signin')
 def profile(request):
     return render(request, 'app/profile.html')
+
 
 def admin_login(request):
     if request.method == "POST":
@@ -32,37 +34,45 @@ def admin_login(request):
         if user is not None:
             login(request, user)
             if request.user.is_superuser:
-                return redirect("/profile")
+                return redirect('profile')
             else:
-                return HttpResponse("You are not an admin.")
+                messages.error(request, 
+                    "This is an Student account. Please Sign-In here.")
+                return redirect('signin')
         else:
             alert = True
-            return render(request, "app/admin_login.html", {'alert': alert})
+            messages.error(request, "Invalid Username & Password.")
+
     return render(request, "app/admin_login.html")
+
 
 def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        
+
         user = authenticate(username=username, password=password)
 
         if user is not None:
             login(request, user)
 
             if request.user.is_superuser:
-                return HttpResponse("this is an admin id go to <a href='admin_login'>Admin Login</a> or <a href= 'home'>home</a>")
+                messages.error(request, 
+                    "This is an ADMIN account. Please Sign-In here.")
+                return redirect('admin_login')
 
             elif request.user.is_active is FALSE:
-                return HttpResponse("Please activate your account! <a href='home'>Home</a>")
-            
+                messages.error(request, 
+                    "Please activate your account via activation link sent to your registered E-Mail ID. Contact ADMIN if any problem persists.")
+
             else:
-                return redirect("/profile")
+                return redirect('profile')
 
         else:
             alert = True
-            return render(request, "app/signin.html", {'alert': alert})
+            messages.error(request, "Invalid Username & Password.")
     return render(request, 'app/signin.html')
+
 
 def signup(request):
     if request.method == "POST":
@@ -73,44 +83,54 @@ def signup(request):
         confirm_password = request.POST['confirm_password']
 
         if User.objects.filter(username=username):
-            messages.error(request, "Username already exist! Please try some other username.")
+            messages.error(
+                request, "Username already exist! Please try some other username.")
             return redirect('signuppage')
 
-        if len(username)>20:
-            messages.error(request, "Username must be under 20 charcters!!")
+        if len(username) > 20:
+            messages.error(request, "Username must be under 20 characters.")
             return redirect('signuppage')
 
         if not username.isalnum():
-            messages.error(request, "Username must be Alpha-Numeric!!")
+            messages.error(request, "Username must be alpha-numeric.")
             return redirect('signuppage')
-        
+
+        if not username.islower():
+            messages.error(request, "Username must be in lowercase.")
+            return redirect('signuppage')
+
         if User.objects.filter(email=email).exists():
-            messages.error(request, "Email Already Registered!!")
+            messages.error(request, "E-Mail ID already registered.")
             return redirect('signinpage')
 
         if password != confirm_password:
-            messages.error(request, "Password didn't matched!")
+            messages.error(request, "Password didn't matched.")
             return redirect('signuppage')
 
-        user = User.objects.create_user(username=username, email=email, password=password)
+        user = User.objects.create_user(
+            username=username, email=email, password=password)
         user.is_active = False
         user.save()
         student = Student.objects.create(user=user, roll_no=roll_no)
         student.save()
         alert = True
 
-        #Confirmation E-mail
+        # Confirmation E-mail
         uidb64 = urlsafe_base64_encode(force_bytes(user.pk))
         token = generate_token.make_token(user)
         current_site = get_current_site(request)
         subject = "Welcome to LMS!"
-        message = "Hello " + user.username + "!\n\n" + "Welcome to LMS. Your account has been created successfully. Please click the link below to activate your account.\n\nConfirmation Link: http://" + current_site.domain + "/activate/" + uidb64 + "/" + token + "\n\nRegards\nTeam LMS"
+        message = "Hello " + user.username + "!\n\n" + "Welcome to LMS. Your account has been created successfully. Please click the link below to activate your account.\n\nConfirmation Link: http://" + \
+            current_site.domain + "/activate/" + uidb64 + \
+            "/" + token + "\n\nRegards\nTeam LMS"
         from_email = settings.EMAIL_HOST_USER
         to_list = [user.email]
-        send_mail(subject , message, from_email, to_list, fail_silently=True)
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
 
-        return render(request, "app/signup.html", {'alert': alert})
+        messages.success(request, 
+            "Activation E-Mail sent to your registered E-Mail address.")
     return render(request, "app/signup.html")
+
 
 def activate(request, uidb64, token):
     try:
@@ -128,12 +148,15 @@ def activate(request, uidb64, token):
     else:
         return render(request, 'activation_failed.html')
 
+
 def logoutUser(request):
     logout(request)
     return redirect('home')
 
+
 def forgetpass(request):
     return render(request, 'app/forgetpass.html')
+
 
 @login_required(login_url='/admin_login')
 def addbook(request):
@@ -144,10 +167,11 @@ def addbook(request):
         category = request.POST['category']
         profile_pic = request.FILES['profile_pic']
 
-        books = Book.objects.create(name=name, author=author, isbn=isbn, category=category, profile_pic=profile_pic)
+        books = Book.objects.create(
+            name=name, author=author, isbn=isbn, category=category, profile_pic=profile_pic)
         books.save()
         alert = True
-        return render(request, "app/addbook.html", {'alert': alert})
+        messages.success(request, "Book is added successfully.")
     return render(request, "app/addbook.html")
 
 
@@ -158,7 +182,8 @@ def viewbook(request):
     if request.GET.get('search_query'):
         search_query = request.GET.get('search_query')
 
-    books = Book.objects.filter(Q(name__icontains=search_query) | Q(author__icontains=search_query) | Q(category__icontains=search_query))
+    books = Book.objects.filter(Q(name__icontains=search_query) | Q(
+        author__icontains=search_query) | Q(category__icontains=search_query))
 
     return render(request, "app/viewbook.html", {'books': books})
 
@@ -166,7 +191,7 @@ def viewbook(request):
 def delete_book(request, myid):
     books = Book.objects.filter(id=myid)
     books.delete()
-    return redirect("viewbook")
+    return redirect('viewbook')
 
 
 @login_required(login_url="/admin_login")
@@ -184,7 +209,7 @@ def view_student(request):
 def delete_student(request, myid):
     students = Student.objects.filter(id=myid)
     students.delete()
-    return redirect("/view_student")
+    return redirect('view_student')
 
 
 def editprofile(request):
@@ -204,26 +229,30 @@ def issuebook_view(request):
             return render(request, 'app/profile.html')
     return render(request, 'app/issuebook.html', {'form': form})
 
+
 def viewissuebook_view(request):
     issuedbooks = IssuedBook.objects.all()
-    li=[]
+    li = []
     for ib in issuedbooks:
-        issdate=str(ib.issuedate.day)+'-'+str(ib.issuedate.month)+'-'+str(ib.issuedate.year)
-        expdate=str(ib.expirydate.day)+'-'+str(ib.expirydate.month)+'-'+str(ib.expirydate.year)
-        #fine calculation
-        days=(date.today()-ib.issuedate)
+        issdate = str(ib.issuedate.day)+'-' + \
+            str(ib.issuedate.month)+'-'+str(ib.issuedate.year)
+        expdate = str(ib.expirydate.day)+'-' + \
+            str(ib.expirydate.month)+'-'+str(ib.expirydate.year)
+        # fine calculation
+        days = (date.today()-ib.issuedate)
         print(date.today())
-        d=days.days
-        fine=0
-        if d>15:
-            day=d-15
-            fine=day*10
-        books=list(Book.objects.filter(isbn=ib.isbn))
-        students=list(Student.objects.filter(roll_no=ib.roll_no))
-        i=0
+        d = days.days
+        fine = 0
+        if d > 15:
+            day = d-15
+            fine = day*10
+        books = list(Book.objects.filter(isbn=ib.isbn))
+        students = list(Student.objects.filter(roll_no=ib.roll_no))
+        i = 0
         for l in books:
-            t=(students[i].user,students[i].roll_no,books[i].name,books[i].author,issdate,expdate,fine)
-            i=i+1
+            t = (students[i].user, students[i].roll_no,
+                 books[i].name, books[i].author, issdate, expdate, fine)
+            i = i+1
             li.append(t)
 
-    return render(request,'app/viewissuebook.html',{'li':li})
+    return render(request, 'app/viewissuebook.html', {'li': li})
--- lms/settings.py
+++ lms/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('email_host_user')
-EMAIL_HOST_PASSWORD = config('email_host_password')
+EMAIL_HOST_USER = load_dotenv('email_host_user')
+EMAIL_HOST_PASSWORD = load_dotenv('email_host_password')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('secret_key')
+SECRET_KEY = load_dotenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
