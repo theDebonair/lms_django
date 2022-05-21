a9daa81ce35c589c9e54f296e25d6b2b14c2c1f5
theDebonair <harshit4277448@live.com>
2022-05-21 17:09:56 +0530

    Various bug fixes.

--- lms/settings.py
+++ lms/settings.py
@@ -11,29 +11,32 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#E-Mail configuration
+# E-Mail configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = load_dotenv('email_host_user')
-EMAIL_HOST_PASSWORD = load_dotenv('email_host_password')
+EMAIL_HOST_USER = 'lmsproject016@gmail.com'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = load_dotenv('secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['lmsapp.pythonanywhere.com', ]
+if not DEBUG:
+    ALLOWED_HOSTS = ['lmsapp.pythonanywhere.com']
 
+else:
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -124,7 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Base url to serve media files
 MEDIA_URL = 'images/'
