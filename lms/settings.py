cb2a35aff24c8adfed57caf12f12c9f35d8fa967
Harshit Gupta <harshit4277448@live.com>
2022-08-19 22:27:56 +0530

    Minor fix

--- lms/settings.py
+++ lms/settings.py
@@ -18,7 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # E-Mail configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
 EMAIL_HOST = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp-mail.outlook.com'
